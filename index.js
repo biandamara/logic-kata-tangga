679d23b2791409fd10c1505dfcfaff2b8bab7919
Bian Damara <99477664+biandamara@users.noreply.github.com>
2023-01-03 10:48:46 +0700

    Update

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ function tangga(kata) {
   console.log(kata);
 }
 
-// variable argumen
+// penggunaan variable argumen
 tangga("foxie");
 tangga("3oz4spoon");
 tangga("087840");
+
+/* Berikut adalah penjelasan langkah-langkah kode di atas:
+1. Membuat sebuah function bernama tangga dengan parameter kata yang akan digunakan sebagai input dari function ini.
+2. Memulai sebuah looping dengan variabel i sebagai counter. Looping akan dilakukan sebanyak panjang kata, lalu setiap iterasi akan mengurangi 1 dari panjang kata.
+3. Pada setiap iterasi, cetak sebanyak i spasi ke console. Kemudian, cetak bagian kata yang tersisa setelah dipotong dari depannya, lalu balikkan kata tersebut.
+4. Setelah looping selesai, cetak kata yang diberikan ke console.
+5. Setelah function selesai dijalankan, maka akan muncul output yang diinginkan. */
